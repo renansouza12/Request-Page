2c092f8c6b4555221d484061f02b7fbbd81a2b77
RenanSouza <renansouza0122@gmail.com>
2023-04-15 15:05:00 -0300

    card stylized and function do add card added

--- js/main.js
+++ js/main.js
@@ -47,3 +47,24 @@ function formNewTitle(e){
 
 
 const btnAdd = document.querySelector('.icon_add');
+
+
+btnAdd.addEventListener('click', addCard);
+
+function addCard(){
+    const cardZone = document.querySelector('.cards');  
+    cardZone.innerHTML += `
+    <div class="card">
+        <div class="card_header">
+            <input type="text" class="title_card" placeholder="Title">
+            <div class="icon_remove" title="remove Card">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </div>
+        </div>
+    <textarea id="card_area" cols="30" rows="10" placeholder="Write here"></textarea>
+  </div> 
+    `  
+}

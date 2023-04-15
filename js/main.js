bbe5e7a82cf273e5be147b087d01169a9ad247f9
RenanSouza <renansouza0122@gmail.com>
2023-04-15 16:56:53 -0300

    function to remove cards added

--- js/main.js
+++ js/main.js
@@ -51,20 +51,34 @@ const btnAdd = document.querySelector('.icon_add');
 
 btnAdd.addEventListener('click', addCard);
 
+const  cardZone = document.querySelector('.cards');   
+
+
 function addCard(){
-    const cardZone = document.querySelector('.cards');  
     cardZone.innerHTML += `
     <div class="card">
         <div class="card_header">
             <input type="text" class="title_card" placeholder="Title">
             <div class="icon_remove" title="remove Card">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
+                stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </div>
         </div>
     <textarea id="card_area" cols="30" rows="10" placeholder="Write here"></textarea>
   </div> 
-    `  
+        `
+        
+    const btn_delets = document.querySelectorAll('.icon_remove');
+
+    btn_delets.forEach(btn_delet => btn_delet.addEventListener('click', removeCard));
+
+      
+}   
+
+function removeCard(){
+    this.parentNode.parentNode.remove();
 }
+
+

fa99020acdfe080862131ca28298b3d3bbd96a99
RenanSouza <renansouza0122@gmail.com>
2023-04-23 14:51:13 -0300

    card style updated

---
+++ js/card.js
@@ -0,0 +1,118 @@
+
+const numberCard = document.querySelector('.number_card'); 
+const cardContainer =  document.querySelector('.card_zone');
+const Allcards = document.querySelectorAll('.cards');
+const dropzone = document.querySelectorAll('.card_zone');
+const AllnumberCards = document.querySelectorAll('.number_card');
+
+let cardCurrent ; 
+
+const btnAdd = document.querySelector('.icon_add');
+btnAdd.addEventListener('click', addCard);
+ 
+
+function addCard(){
+      numberCard.innerHTML = cardContainer.children.length;
+    
+    cardContainer.innerHTML += `
+    <div class="card" draggable='true' >
+        <div class="card_header">
+            <input type="text" class="title_card" placeholder="Title" readonly>
+            <div class="icon_remove" title="remove Card">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </div>
+        </div>
+    <textarea id="card_area" cols="30" rows="10" placeholder="Write here" readonly></textarea>
+    <div class="edit_card">
+    <div class="edit">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
+</svg>      
+<span id="text_btnEdit">Edit</span></div>
+      
+    </div>
+  </div> 
+        `   
+        dropzone.forEach((zone, index)=>{
+            AllnumberCards[index].innerHTML = zone.children.length;
+        })
+   
+
+    document.querySelectorAll('.icon_remove').forEach(btn_delet => btn_delet.addEventListener('click', removeCard));
+    document.querySelectorAll('.edit_card').forEach(btnEdit => btnEdit.addEventListener('click',editCard));
+    
+   
+    
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('dragstart',dragstart);
+        card.addEventListener('dragend', dragend);
+    })
+
+}   
+
+function dragstart(e){
+    dropzone.forEach(dropzone => dropzone.classList.add('active'));
+    cardCurrent = e.target;
+    this.style.cursor = 'move'; 
+}
+
+function dragend(){
+     dropzone.forEach(dropzone => dropzone.classList.remove('active'));
+    
+}
+
+dropzone.forEach(dropzone =>{
+    dropzone.addEventListener('dragover',dragover);
+    dropzone.addEventListener('dragleave',dragleave);
+})
+
+function dragover(){
+    this.classList.add('over');
+    const zones = document.querySelectorAll('.cards');
+    this.appendChild(cardCurrent);
+    
+    dropzone.forEach((zone, index)=>{
+        AllnumberCards[index].innerHTML = zone.children.length;
+    })
+}
+function dragleave(){
+    this.classList.remove('over');
+}
+
+function editCard(e){
+    const clickedCard = e.target.closest('.card');
+    const inputTitle = clickedCard.querySelector('.title_card');
+    const textarea = clickedCard.querySelector('textarea');
+    const btnTextEdit = clickedCard.querySelector('#text_btnEdit');
+
+    textarea.removeAttribute('readonly');
+    if(inputTitle.readOnly === true){
+        inputTitle.removeAttribute('readonly');
+        textarea.removeAttribute('readonly');
+        inputTitle.classList.add('active');
+        textarea.classList.add('active');
+        btnTextEdit.innerHTML = 'Save';
+
+    }else{
+        inputTitle.setAttribute('readonly',true);
+        textarea.setAttribute('readonly',true);
+        inputTitle.classList.remove('active');
+        textarea.classList.remove('active');
+        btnTextEdit.innerHTML = 'Edit';
+    }
+}   
+
+function removeCard(){
+    this.parentNode.parentNode.remove();
+    
+    dropzone.forEach((zone, index)=>{
+        AllnumberCards[index].innerHTML = zone.children.length;
+    })
+
+}
+
+
---
+++ js/title.js
@@ -0,0 +1,45 @@
+const btn_edit =  document.querySelector('.icon_edit');
+const formTitle = document.querySelector('.new_title');
+const currentTitle = document.querySelector('#title');
+// click event added to icon and run changeTitle function
+btn_edit.addEventListener('click',changeTitle);
+
+//  click event added to body and run removeChangeTitle function
+document.addEventListener('click', removeChangeTitle)
+
+//submit event added to class="new_title" and run formNewTitle function
+formTitle.addEventListener('submit',formNewTitle);
+
+
+//function to add and remove class "active" to the class "new_title" 
+function changeTitle(e){
+
+    formTitle.classList.toggle('active');
+    currentTitle.classList.toggle('active');
+}
+
+//function to remove class active to the class "new_title"  when clicked outside
+function removeChangeTitle(e){
+
+    // validation in case the  click event  was clicked outside
+    if(!btn_edit.contains(e.target) && !formTitle.contains(e.target)){
+        formTitle.classList.remove('active');
+        currentTitle.classList.remove('active');
+    }
+    
+}
+
+// function to add the input value to the current title and clean the input 
+function formNewTitle(e){  
+    
+    const inputTitle = document.querySelector('.input_title');
+
+    // validation in case  input is empty 
+    inputTitle.value < 1 ? alert('Name Invalid 🚨‼️🟠') : currentTitle.innerHTML = inputTitle.value;
+    
+
+    //clean the input 
+    inputTitle.value = '';
+
+    e.preventDefault();
+}
